8680f2e4fbf4582c0a017a8025e7381a265988cc
Jakob Klocker <jklocker@c2r2p4.42vienna.com>
2023-01-12 17:41:46 +0100

    test

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
-int main(int argc, char **argv, char **envp)
+#include "minishell.h"
+
+int	main(void) //int argc, char **argv, char **envp)
 {
-    
+	char *input = malloc(100);
+	size_t n = 100;
+	getline(&input, &n, stdin);
+    printf("%i", count_words(input));
 }
--- node_funcs.c
+++ node_funcs.c
@@ -4,9 +4,10 @@ void    init_nodes(t_node *head)
 {
     while(head)
     {
-        head->full_cmd = 0;
-        head->full_path = 0;
+        head->full_cmd = NULL;
+        head->full_path = NULL;
         head->in = 0;
         head->out = 1;
         head = head->next;
+    }
 }
