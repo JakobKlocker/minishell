c5c5cc9d0faaf967a5ca05f9b05049e8e1a8a7d4
Jakob Klocker <jklocker@c2r2p4.42vienna.com>
2023-01-12 16:01:19 +0100

    test

--- minishell.h
+++ minishell.h
@@ -1,5 +1,17 @@
-#include libft.h
+#include "libft/libft.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <dirent.h>
+#include <string.h>
+#include <signal.h>
+
+typedef struct s_node
+{
+	char	**full_cmd;
+	char	*full_path;
+	int     in;
+	int	    out;
+    void    *next;
+}		t_node;
