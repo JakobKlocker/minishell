81c5d9549ef440592fb2f70e4cfc025a9df8472a
Jakob Klocker <jklocker@c2r2p4.42vienna.com>
2023-01-16 17:46:15 +0100

    Added expander

--- env_funcs.c
+++ env_funcs.c
@@ -1,32 +1,36 @@
 #include "minishell.h"
 
-int	main(int argc, char **argv, char **envp)
-{
-    t_info info;
-    if (copy_env(&info, envp) == 1)
-        exit (0);
-}
+// int	main(int argc, char **argv, char **envp)
+// {
+//     t_info info;
+//     if (copy_env(&info, envp) == 1)
+//         exit (0);
+// }
 
 int    copy_env(t_info *info, char **envp)
 {
     int i;
     int len;
+    envlst_t * head;
 
-    envlst_t * head = NULL;
     head = (envlst_t *)malloc(sizeof(envlst_t));
-        if (!head)
-            return (1);
+    if (!head)
+        return (1);
     info->envp = head;
     i = 0;
     while (envp[i])
     {
+        if(i != 0)
+        {
+            head->next = (envlst_t *)malloc(sizeof(envlst_t));
+            head = head->next;
+        }
         len = ft_strlen(envp[i]);
         head->var = malloc(len + 1);
         memcpy(head->var, envp[i], len);
         head->var[len] = '\0';
-        head->next = NULL;
-        head = head->next;
         i++;
     }
+    head->next = NULL;
     return (0);
 }
